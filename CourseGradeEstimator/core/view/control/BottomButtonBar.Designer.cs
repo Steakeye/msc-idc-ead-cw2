d34d9991abda52ec89d6b63081070ee57fabe120
Andrew Keats <steakeye.studios@gmail.com>
2017-12-08 19:07:09 +0000

    layout of layout panel in button bar

--- CourseGradeEstimator/core/view/control/BottomButtonBar.Designer.cs
+++ CourseGradeEstimator/core/view/control/BottomButtonBar.Designer.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows.Forms;
+using System.Drawing;
 
 namespace CourseGradeEstimator.core.view.control
 {
@@ -41,11 +42,13 @@ namespace CourseGradeEstimator.core.view.control
             layoutPanel.Size = new System.Drawing.Size(0, 50);
             //layoutPanel.MaximumSize = Size;
             layoutPanel.MinimumSize = Size;
-            layoutPanel.Location = Config.Dimensions.GetStandardStartLocation();
+            layoutPanel.Location = System.Drawing.Point.Add(Config.Dimensions.GetStandardStartLocation(), new Size(0, 10)); //new System.Drawing.Point(30,30); //Config.Dimensions.GetStandardStartLocation();
             //layoutPanel.Anchor = AnchorStyles.Left | AnchorStyles.Right | AnchorStyles.Bottom;
             layoutPanel.Anchor = AnchorStyles.Left | AnchorStyles.Right;
             //layoutPanel.Dock = DockStyle.Fill;
             layoutPanel.BackColor = System.Drawing.Color.BlueViolet;
+            layoutPanel.Margin = Config.Dimensions.GetStandardSidePadding();
+
             //layoutPanel.Margin = new Padding(30);
             //layoutPanel.Padding = new Padding(30);
 
