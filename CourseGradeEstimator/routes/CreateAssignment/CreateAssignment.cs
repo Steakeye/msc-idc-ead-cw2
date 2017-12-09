af77102868692ba3a69424b4a9b257d92fb36388
Andrew Keats <steakeye.studios@gmail.com>
2017-12-09 02:22:17 +0000

    adding new create assignment views

---
+++ CourseGradeEstimator/routes/CreateAssignment/CreateAssignment.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CourseGradeEstimator.routes.CreateAssignment
+{
+    public partial class CreateAssignment : core.view.CreateSimple.CreateSimple
+    {
+        public CreateAssignment(): base()
+        {
+            InitializeComponent();
+            string headerTitle = Properties.Resources.ViewCreateModuleHeader;
+            this.Text = $"{Properties.Resources.AppTitle} - Create {headerTitle}";
+            this.header.Title = headerTitle;
+            header.BackColor = Color.Orange;
+            //childItems.BackColor = Color.Blue;
+
+            bottomButtonBar.BackColor = Color.Green;
+            //childItems.Width = Width;
+            tablePanel.BackColor = Color.LightBlue;
+            BackColor = Color.HotPink;
+        }
+    }
+}
